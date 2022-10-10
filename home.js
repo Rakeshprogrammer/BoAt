07ade79797724904be82e13094cd70ce4c248de0
Rakeshprogrammer <webprogrammerrakesh@gmail.com>
2022-10-10 01:55:33 -1200

    light dark mode and coupon code done

---
+++ home.js
@@ -0,0 +1,11 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.body.style.opacity="0.9"
+    
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.body.style.opacity="1"
+   
+}
